57d3782f2833765d046a628c495ea99b76e8fda0
nhatdang2604 <nhatdang2604@gmail.com>
2022-03-25 16:10:19 +0700

    Build DFS

--- Lab01/student_functions.py
+++ Lab01/student_functions.py
@@ -1,9 +1,46 @@
 import numpy as np
 
+def DFS_getVisited(matrix, currentNode, end, visited):
+    
+    #terminate condition for recursive
+    if (currentNode == end):
+        return visited
+    
+    #recursive part
+    for node in matrix[currentNode]:
+        if node not in visited.keys():
+            visited[node] = currentNode
+            return DFS_getVisited(matrix, node, end, visited)
 
+    #return the current visisted, if there are:
+    #   0) The current node has no adjacency node
+    #   1) All the adjacency node of current node is visited
+    return visited
+
+#Trace the path from start to end, 
+# by using the builded visited
+def DFS_getPath(start, end, visited):
+
+    #the answer path
+    path: list
+
+    #Iterate from end -> start
+    nodeIterator = end
+
+    #Trace from end -> start (because visited[start] = None)
+    while nodeIterator != None:
+        path.append(nodeIterator)
+        nodeIterator = visited[nodeIterator]
+
+    #Reverse the path to become start -> end
+    path.reverse()
+
+    #Return the reversed path
+    return path
+    
 def DFS(matrix, start, end):
     """
-    BFS algorithm:
+    FS algorithm:
     Parameters:
     ---------------------------
     matrix: np array 
@@ -26,11 +63,21 @@ def DFS(matrix, start, end):
     path=[]
     visited={}
 
+    #init the start node's visitor
+    visited[start] = None
+
+    #Build the visited dictionary
+    visited = DFS_getVisited(matrix, start, end, visited)
+    
+    #Only trace the path, if there is any path from start -> end
+    if end in visited.keys():
+        path = DFS_getPath(start, end, visited)
+
     return visited, path
 
 def BFS(matrix, start, end):
     """
-    DFS algorithm
+    BFS algorithm
      Parameters:
     ---------------------------
     matrix: np array 
