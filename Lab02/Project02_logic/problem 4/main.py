9472ea5b29b42d90b2799c9bf049eaedcbf13035
nhatdang2604 <nhatdang2604@gmail.com>
2022-05-02 23:25:41 +0700

    finish 70% problem 4

--- Lab02/Project02_logic/problem 4/main.py
+++ Lab02/Project02_logic/problem 4/main.py
@@ -1,11 +1,20 @@
 from source.FileReader import FileReader
+from source.Solver import Literal, Clause, Solver
 
 def main() -> None:
 
     #TODO:
-    reader = FileReader('/input', '/output')
-    reader.inputFiles()
-    reader.outputFiles()
+    # reader = FileReader('/input', '/output')
+    # reader.inputFiles()
+    # reader.outputFiles()
+
+    literals = {Literal('A'), Literal('-A'), Literal('-B'), Literal('-C'), Literal('----B'), Literal('--B')}
+    clause = Clause(literals, True)
+    ls = []
+
+    solver = Solver(clause, ls)
+    solver.addNotAlphaIntoKB()
+    #print(clause)
 
 if __name__ == '__main__':
     main()
--- Lab02/Project02_logic/problem 4/source/Solver.py
+++ Lab02/Project02_logic/problem 4/source/Solver.py
@@ -10,18 +10,24 @@ class Literal:
     def __hash__(self):
         return hash(self.__value)
     
-    def __str__(self):
+    def __str__(self) -> str:
         return self.__value
+
+    def __eq__(self, other: 'Literal'):
+        return self.__value == other.value()
+
+    def __lt__(self, other: 'Literal'):
+        return self.value() < other.value()
 
     def value(self) -> str:
         return self.__value
 
     #Check if the literal is positive
     def isPositive(self) -> bool:
-        numberOfMinus = self__value.count('-')
-
+        numberOfMinus = self.__value.count('-')
+        
         #If the number of minus is odd => not positive
-        if 1 == numberOfMinus&1:
+        if 1 == numberOfMinus & 1:
             return False
         return True
 
@@ -30,36 +36,133 @@ class Literal:
     #               ---A = -A
     def reduce(self) -> None:
 
-        #Delete all minus in literal
-        self.__value = self.__value.replace('-', '')
-
         if not self.isPositive():
-            self.__value = '-'.join(self.__value)
+            self.__value = self.__value.replace('-', '')
+            self.__value = '-' + self.__value
+        else:
+            self.__value = self.__value.replace('-', '')
+
         
-    def inverse(self) -> Literal:
-        return Literal('-'.join(self.__value ))
+    def inverse(self) -> 'Literal':
+        return Literal('-' + self.__value)
+
+    #return true if (literal Or Not(literal))
+    #   else: false
+    def Or(self, literal: 'Literal') -> bool:
+
+        inverseForm = self.inverse()
+        if inverseForm == literal:
+            return True
+
+        return False
 
 class Clause:
     __literals: set
+    __isAppliable: bool
 
-    def __init__(self, literals: set) -> None:
+    def __init__(self, literals: set, isAppliable: bool) -> None:
         self.__literals = literals
+        self.__isAppliable = isAppliable
+
+    def __hash__(self):
+        hashableSet = frozenset(self.__literals)
+        return hash(hashableSet)
+
+    def __str__(self) -> str:
+        needle = ' OR '
+
+        return needle.join(literal.value() for literal in self.__literals)
+
 
     def literals(self) -> set:
         return self.__literals
 
-    def Or(self, literal: Literal) -> None:
+    def isAppliable(self) -> bool:
+        return self. __isAppliable
 
-        #Check if there are an inverse of the given literal
-        inverse = literal.inverse()
+    def isEmpty(self) -> bool:
+        return 0 == len(self.__literals)
 
-        #Add the literal if there is no inverse form in the clause, else remove the inverse form
-        if inverse not in self.__literals:
-            self.__literals.add(literal)
-        else:
-            self.__literals.remove(inverse)
+    def resolve(self, other: 'Clause') -> 'Clause':
+
+        newClauseLiteral = self.literals().union(other.literals())
+        
+        #delete the 1st counter literals
+        for literal in newClauseLiteral:
+            inverse = literal.inverse()
+            if inverse in newClauseLiteral:
+                newClauseLiteral.remove(literal)
+                newClauseLiteral.remove(inverse)
+                break
+        
+        for literal in newClauseLiteral:
+            inverse = literal.inverse()
+            if inverse in newClauseLiteral:
+                return Clause(newClauseLiteral, False)
+        
+        return Clause(newClauseLiteral, True)
+
+        
+    def isAbleToResolve(self, other: 'Clause') -> bool:
+
+        for literal in self.__literals:
+            if literal.inverse() in other.literals():
+                return True
+
+        return False
+
+
+class Solver:
+    __alpha: Clause
+    __kb: list
+    __stepResult: list
+
+    def __init__(self, alpha: Clause, kb: list) -> None:
+        self.__alpha = alpha
+        self.__kb = kb
+
+    def setAlpha(self, alpha: Clause) -> None:
+        self.__alpha = alpha
     
-    def Or(self, clause: Clause) -> None:
-        literals = clause.literals()
+    def setKB(self, kb: list) -> None:
+        self.__kb = kb
+
+    def addNotAlphaIntoKB(self) -> None:
+
+        #split literal from literals,add to list, then sort it (because set is not ordered)
+        literals = [literal.inverse() for literal in self.__alpha.literals()]
+        literals.sort()
+        
+        #Add the literal from alpha to kb
         for literal in literals:
-            self.Or(literal)
+            self.__kb.append(literal)
+    
+    def pl_resolution(self) -> str:
+        self.addNotAlphaIntoKB()
+
+        resolvent: Clause
+        
+
+        while True:
+            resolvents = []
+            for i in range(0, len(kb) - 1):
+                for j in range(i + 1, len(kb)):
+                    if kb[i].isAbleToResolve(kb[j]):
+                        
+                        resolvent = kb[i].resolve(kb[j])
+
+                        #Check if the resolvent is not the True clause
+                        #   => add to KB
+                        if resolvent.isAppliable():
+                            resolvents.append(resolvent)
+
+            
+            self.__stepResult.append(resolvents)
+            for resolv in resolvents:
+                if resolv.isEmpty():
+                    return "YES"
+
+
+
+                        
+        
